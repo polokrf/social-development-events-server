e28ef5afa727d2c9d6b3a65ae8379b97f9ed506a
polokrf <polokkumar9030@gmail.com>
2025-11-09 07:47:25 +0600

    add mongodb

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors')
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -8,12 +11,41 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// social-events
+// clMQwUIrbnfo1bNK
 
+const uri = `mongodb+srv://${process.env.Mongodb_Name}:${process.env.Mongodb_Key}@cluster0.jkj46mi.mongodb.net/?appName=Cluster0`;
+
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
 
 
 app.get('/', (req, res) => {
   res.send('hello i am social events')
 })
+
+
+
+async function run() {
+  try {
+   
+    await client.connect();
+    
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+   
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log('i am social port ' ,port)
